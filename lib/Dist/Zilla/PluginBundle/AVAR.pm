5e88e519e1b4f6e18ee3c4e68f639532eade7917
Ævar Arnfjörð Bjarmason <avar@cpan.org>
2013-09-21 05:51:00 +0000

    Import of AVAR/Dist-Zilla-PluginBundle-AVAR-0.29 from CPAN.
    gitpan-cpan-distribution: Dist-Zilla-PluginBundle-AVAR
    gitpan-cpan-version:      0.29
    gitpan-cpan-path:         AVAR/Dist-Zilla-PluginBundle-AVAR-0.29.tar.gz
    gitpan-cpan-author:       AVAR
    gitpan-cpan-maturity:     released

--- lib/Dist/Zilla/PluginBundle/AVAR.pm
+++ lib/Dist/Zilla/PluginBundle/AVAR.pm
@@ -3,7 +3,7 @@ BEGIN {
   $Dist::Zilla::PluginBundle::AVAR::AUTHORITY = 'cpan:AVAR';
 }
 {
-  $Dist::Zilla::PluginBundle::AVAR::VERSION = '0.28';
+  $Dist::Zilla::PluginBundle::AVAR::VERSION = '0.29';
 }
 
 use 5.10.0;
@@ -44,11 +44,11 @@ sub bundle_config {
     my $cpan_id = try { $zilla->stash_named('%PAUSE')->username };
 
     my $authority   = $args->{authority} // ($cpan_id ? "cpan:$cpan_id" : 'cpan:AVAR');
+    my $no_Authority = $args->{no_Authority} // 0;
     my $no_a_pre    = $args->{no_AutoPrereq} // 0;
     my $use_mm      = $args->{use_MakeMaker} // 1;
     my $use_ct      = $args->{use_CompileTests} // $args->{use_TestCompile} // 1;
     my $bugtracker  = $args->{bugtracker}  // 'rt';
-    warn "AVAR: Don't use GitHub as a tracker" if $bugtracker eq 'github';
     my $homepage    = $args->{homepage};
     warn "AVAR: Upgrade to new format" if $args->{repository};
     my $repository_url  = $args->{repository_url};
@@ -138,12 +138,17 @@ sub bundle_config {
             }
 
         ],
-        [
-            Authority => {
-                authority   => $authority,
-                do_metadata => 1,
-            }
-        ],
+        ($no_Authority
+         ? ()
+         : (
+             [
+                 Authority => {
+                     authority   => $authority,
+                     do_metadata => 1,
+                 }
+             ]
+          )
+        ),
         # Bump the Changlog
         [
             NextRelease => {
@@ -225,6 +230,7 @@ This is the plugin bundle that AVAR uses. Use it as:
     use_TestCompile = 0 ; I have my own compile tests here..
     ;; cpan:YOUR_CPAN_ID is the default authority, read from "dzil setup" entry for PAUSE
     authority = cpan:AVAR
+    ; no_Authority = 1 ; If you don't want to use the authority module
     ;; if you want to install your dist after release (set $ENV{PERL_CPANM_OPTS} if you need --sudo or --mirror etc.)
     ;; default is OFF
     install_command = cpanm .
