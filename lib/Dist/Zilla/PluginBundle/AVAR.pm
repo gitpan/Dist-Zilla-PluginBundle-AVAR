86bf334ecd6bde450f121ebf845e444da60059ae
Rafael Kitover <rkitover@gmail.com>
2014-05-18 15:12:46 +0000

    Import of RKITOVER/Dist-Zilla-PluginBundle-AVAR-0.29 from CPAN.
    gitpan-cpan-distribution: Dist-Zilla-PluginBundle-AVAR
    gitpan-cpan-version:      0.29
    gitpan-cpan-path:         RKITOVER/Dist-Zilla-PluginBundle-AVAR-0.29.tar.gz
    gitpan-cpan-author:       RKITOVER
    gitpan-cpan-maturity:     released

--- lib/Dist/Zilla/PluginBundle/AVAR.pm
+++ lib/Dist/Zilla/PluginBundle/AVAR.pm
@@ -2,10 +2,7 @@ package Dist::Zilla::PluginBundle::AVAR;
 BEGIN {
   $Dist::Zilla::PluginBundle::AVAR::AUTHORITY = 'cpan:AVAR';
 }
-{
-  $Dist::Zilla::PluginBundle::AVAR::VERSION = '0.30';
-}
-
+$Dist::Zilla::PluginBundle::AVAR::VERSION = '0.29';
 use 5.10.0;
 use Moose;
 use Moose::Autobox;
@@ -44,11 +41,11 @@ sub bundle_config {
     my $cpan_id = try { $zilla->stash_named('%PAUSE')->username };
 
     my $authority   = $args->{authority} // ($cpan_id ? "cpan:$cpan_id" : 'cpan:AVAR');
-    my $no_Authority = $args->{no_Authority} // 0;
     my $no_a_pre    = $args->{no_AutoPrereq} // 0;
     my $use_mm      = $args->{use_MakeMaker} // 1;
     my $use_ct      = $args->{use_CompileTests} // $args->{use_TestCompile} // 1;
     my $bugtracker  = $args->{bugtracker}  // 'rt';
+    warn "AVAR: Don't use GitHub as a tracker" if $bugtracker eq 'github';
     my $homepage    = $args->{homepage};
     warn "AVAR: Upgrade to new format" if $args->{repository};
     my $repository_url  = $args->{repository_url};
@@ -138,17 +135,12 @@ sub bundle_config {
             }
 
         ],
-        ($no_Authority
-         ? ()
-         : (
-             [
-                 Authority => {
-                     authority   => $authority,
-                     do_metadata => 1,
-                 }
-             ]
-          )
-        ),
+        [
+            Authority => {
+                authority   => $authority,
+                do_metadata => 1,
+            }
+        ],
         # Bump the Changlog
         [
             NextRelease => {
@@ -183,6 +175,18 @@ sub bundle_config {
              : ()),
         },
     });
+
+    # remove empty entries, Config::MVP crashes on them
+
+    foreach my $p (@plugins) {
+        my ($name, $package, $payload) = @$p;
+
+        while (my ($k, $v) = each %$payload) {
+            if (!$v || (ref $v eq 'ARRAY' && !@$v) || (ref $v eq 'HASH' && !%$v)) {
+                delete $payload->{$k};
+            }
+        }
+    }
 
     return @plugins;
 }
@@ -230,7 +234,6 @@ This is the plugin bundle that AVAR uses. Use it as:
     use_TestCompile = 0 ; I have my own compile tests here..
     ;; cpan:YOUR_CPAN_ID is the default authority, read from "dzil setup" entry for PAUSE
     authority = cpan:AVAR
-    ; no_Authority = 1 ; If you don't want to use the authority module
     ;; if you want to install your dist after release (set $ENV{PERL_CPANM_OPTS} if you need --sudo or --mirror etc.)
     ;; default is OFF
     install_command = cpanm .
@@ -242,7 +245,7 @@ It's equivalent to:
     remove = PodVersion
     remove = PodCoverageTests
     
-    [Git::NextVersion]
+    [VersionFromPrev]
     [AutoPrereqs]
     [MetaJSON]
 
